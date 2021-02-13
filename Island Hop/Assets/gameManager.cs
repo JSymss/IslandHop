0e10428f440bbc3f7e2d6872e25c683fda6ac4e4
Jackson <200341@student.sae.edu.au>
2021-02-13 21:40:52 +1100

    Added Unity project

---
+++ Island Hop/Assets/gameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject MainChar;
+    public GameObject E;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        E.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Island Hop/Assets/treeInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeInteract : MonoBehaviour
+{
+    public GameObject MainChar;
+    public GameObject E;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.name == "MainChar")
+        {
+            E.gameObject.SetActive(true);
+            if (Input.GetKeyDown("e"))
+            {
+                print("You have collided and pressed 'e'");
+                MainChar.GetComponent<playerMovement>().enabled = false;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        E.gameObject.SetActive(false);
+    }
+}
